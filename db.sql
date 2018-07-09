cae7faa83e23b3d9bd93d9727ccdd1de4c9b2a59
cassi <cassi@DESKTOP-UC4PCLI.localdomain>
2018-07-08 22:54:58 -0300

    Atualizado db.sql para banco de dados ir com algumas informações.

--- db.sql
+++ db.sql
@@ -1,151 +1,225 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: trade_games
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema trade_games
--- -----------------------------------------------------
+--
+-- Table structure for table `ads`
+--
 
--- -----------------------------------------------------
--- Schema trade_games
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `trade_games` DEFAULT CHARACTER SET utf8 ;
-USE `trade_games` ;
+DROP TABLE IF EXISTS `ads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` tinytext NOT NULL,
+  `type` tinyint(1) NOT NULL COMMENT '1 = Procura\n2 = Oferta',
+  `price` decimal(10,0) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `games_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`games_id`,`users_id`),
+  KEY `fk_ads_games1_idx` (`games_id`),
+  KEY `fk_ads_users1_idx` (`users_id`),
+  CONSTRAINT `fk_ads_games1` FOREIGN KEY (`games_id`) REFERENCES `games` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ads_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `trade_games`.`games`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`games` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` TINYTEXT NOT NULL,
-  `image` VARCHAR(100) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `ads`
+--
 
+LOCK TABLES `ads` WRITE;
+/*!40000 ALTER TABLE `ads` DISABLE KEYS */;
+INSERT INTO `ads` VALUES (1,'teste',1,12,'2018-04-11 20:39:14',1,1),(2,'teste 2',1,10,'2018-04-11 21:43:38',1,1),(3,'teste 3',2,5,'2018-04-11 21:44:21',1,1),(4,'Descrição testeeeeeeeeeeeeee',1,5,'2018-07-07 21:41:13',7,2),(5,'Descrição teste',1,46,'2018-07-07 21:12:24',7,1),(6,'teste',1,1,'2018-07-07 21:19:38',1,1),(8,'zxcv',1,3,'2018-07-07 21:22:00',1,2),(9,'fdgfdgfdgdfs',2,2,'2018-07-07 21:41:46',7,2);
+/*!40000 ALTER TABLE `ads` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `trade_games`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `full_name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `comments`
+--
 
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `ads_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`ads_id`,`users_id`),
+  KEY `fk_comments_ads1_idx` (`ads_id`),
+  KEY `fk_comments_users1_idx` (`users_id`),
+  CONSTRAINT `fk_comments_ads1` FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `trade_games`.`ads`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`ads` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `description` TINYTEXT NOT NULL,
-  `type` TINYINT(1) NOT NULL COMMENT '1 = Procura\n2 = Oferta',
-  `price` DECIMAL NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `games_id` INT NOT NULL,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `games_id`, `users_id`),
-  INDEX `fk_ads_games1_idx` (`games_id` ASC),
-  INDEX `fk_ads_users1_idx` (`users_id` ASC),
-  CONSTRAINT `fk_ads_games1`
-    FOREIGN KEY (`games_id`)
-    REFERENCES `trade_games`.`games` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ads_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `trade_games`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `comments`
+--
 
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,'teste','2018-04-11 22:00:26',NULL,1,1),(2,'teste teste','2018-04-11 22:11:39',NULL,1,1),(3,'teste teste teste','2018-04-11 22:25:06',NULL,1,1),(4,'teste teste teste teste','2018-04-11 22:30:07',NULL,1,1),(5,'fgsdfdsfds','2018-04-11 22:30:15',NULL,2,1),(6,'asdf','2018-04-12 23:01:24',NULL,1,1),(7,'fdsa','2018-04-12 23:22:46',NULL,1,1),(8,'teste','2018-04-14 11:55:26',NULL,3,1),(9,'gfggfddgs fdgfdgd fsgfdgfd\r\ngfdgfdgfd','2018-04-14 11:56:44',NULL,3,1),(10,'teste','2018-04-23 12:31:40',NULL,3,2),(11,'teste','2018-06-20 22:31:32',NULL,3,1),(12,'teste','2018-07-07 21:40:41',NULL,8,2),(13,'teste','2018-07-07 21:40:58',NULL,4,2),(14,'teste 2','2018-07-07 21:46:13',NULL,4,2),(15,'cassio','2018-07-07 21:47:18',NULL,8,1);
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `trade_games`.`roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`roles` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `role` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `games`
+--
 
+DROP TABLE IF EXISTS `games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` tinytext NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `trade_games`.`users_roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`users_roles` (
-  `users_id` INT NOT NULL,
-  `roles_id` INT NOT NULL,
-  PRIMARY KEY (`users_id`, `roles_id`),
-  INDEX `fk_users_has_roles_roles1_idx` (`roles_id` ASC),
-  INDEX `fk_users_has_roles_users1_idx` (`users_id` ASC),
-  CONSTRAINT `fk_users_has_roles_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `trade_games`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_has_roles_roles1`
-    FOREIGN KEY (`roles_id`)
-    REFERENCES `trade_games`.`roles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `games`
+--
 
+LOCK TABLES `games` WRITE;
+/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+INSERT INTO `games` VALUES (1,'Battlefield V','Jogo de guerra','bf5.jpg'),(7,'Fifa 18','Jogo de futebol','fifa18.jpg');
+/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `trade_games`.`comments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`comments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `comment` TEXT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NULL,
-  `ads_id` INT NOT NULL,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `ads_id`, `users_id`),
-  INDEX `fk_comments_ads1_idx` (`ads_id` ASC),
-  INDEX `fk_comments_users1_idx` (`users_id` ASC),
-  CONSTRAINT `fk_comments_ads1`
-    FOREIGN KEY (`ads_id`)
-    REFERENCES `trade_games`.`ads` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comments_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `trade_games`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `interested`
+--
 
+DROP TABLE IF EXISTS `interested`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interested` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ads_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`ads_id`,`users_id`),
+  KEY `fk_interested_ads1_idx` (`ads_id`),
+  KEY `fk_interested_users1_idx` (`users_id`),
+  CONSTRAINT `fk_interested_ads1` FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_interested_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `trade_games`.`interested`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trade_games`.`interested` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `ads_id` INT NOT NULL,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `ads_id`, `users_id`),
-  INDEX `fk_interested_ads1_idx` (`ads_id` ASC),
-  INDEX `fk_interested_users1_idx` (`users_id` ASC),
-  CONSTRAINT `fk_interested_ads1`
-    FOREIGN KEY (`ads_id`)
-    REFERENCES `trade_games`.`ads` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_interested_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `trade_games`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `interested`
+--
 
+LOCK TABLES `interested` WRITE;
+/*!40000 ALTER TABLE `interested` DISABLE KEYS */;
+INSERT INTO `interested` VALUES (1,1,1),(7,1,2),(6,3,2),(8,3,1),(15,6,2),(9,8,2),(14,8,1);
+/*!40000 ALTER TABLE `interested` ENABLE KEYS */;
+UNLOCK TABLES;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Cássio Izumi Krüger','cassiokruger@gmail.com','123'),(2,'User 1','user1@email.com','123'),(3,'User2','user2@email.com','123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_roles` (
+  `users_id` int(11) NOT NULL,
+  `roles_id` int(11) NOT NULL,
+  PRIMARY KEY (`users_id`,`roles_id`),
+  KEY `fk_users_has_roles_roles1_idx` (`roles_id`),
+  KEY `fk_users_has_roles_users1_idx` (`users_id`),
+  CONSTRAINT `fk_users_has_roles_roles1` FOREIGN KEY (`roles_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_roles_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+LOCK TABLES `users_roles` WRITE;
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+INSERT INTO `users_roles` VALUES (1,1),(2,2),(3,2);
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-08 17:34:48
